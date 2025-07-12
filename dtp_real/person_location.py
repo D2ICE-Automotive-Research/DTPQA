4c76defcab8fcff27602762527b331babfac9080
Nikos Theodoridis <niktheod5150@gmail.com>
2025-07-12 10:23:19 +0100

    Added the first 3 real categories to the annotations file

--- dtp_real/person_location.py
+++ dtp_real/person_location.py
@@ -13,7 +13,7 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--dataroot', type=str, help='Path to the NuScenes dataset root directory.')
     parser.add_argument("--save_path", type=str, help="Path to save the annotations.")
-    parser.add_argument("--camera", type=str, default="CAM_FRONT", help="Camera to use for the dataset.")
+    parser.add_argument("--cameras", type=list, default=["CAM_FRONT", "CAM_BACK", "CAM_FRONT_LEFT", "CAM_FRONT_RIGHT", "CAM_BACK_LEFT", "CAM_BACK_RIGHT"], help="Cameras to use for the dataset.")
     parser.add_argument("--bins", type=list, default=[5, 10, 20, 30, 40, 50], help="Distance bins for the dataset.")
     parser.add_argument("--num_negative_samples", type=int, default=200, help="Number of negative samples")
     parser.add_argument("--question", type=str, default="Where in the image is the person located?")
@@ -36,37 +36,38 @@ def main(args):
     single_pedestrians_data = []
 
     # Keep only the samples with a single human in the scene
-    for sample in tqdm(pedestrian_samples[:1000]):
-        datum = {}
-        human_ann = None
-        cnt = 0
-        cnt2 = 0
-        for ann in sample['anns']:
-            sample_annotation = nusc.get('sample_annotation', ann)
-            if sample_annotation['category_name'] == "human.pedestrian.stroller":
-                    continue
-            if "human" in sample_annotation["category_name"] or "bicycle" in sample_annotation["category_name"] or "motorcycle" in sample_annotation["category_name"]:
-                _, boxes, _ = nusc.get_sample_data(sample['data'][args.camera], box_vis_level=BoxVisibility.ANY, selected_anntokens=[ann])
-                if boxes:
-                    cnt2 += 1
-            if "human" in sample_annotation["category_name"]:
-                _, boxes, _ = nusc.get_sample_data(sample['data'][args.camera], box_vis_level=BoxVisibility.ALL, selected_anntokens=[ann])
-                if boxes and sample_annotation['visibility_token'] == '4':
-                    human_ann = sample_annotation
-                    cnt += 1
-        if cnt == cnt2 == 1:
-            sample_data = nusc.get("sample_data", sample['data'][args.camera])
-            ego_pose_token = nusc.field2token("ego_pose", "timestamp", sample["timestamp"])[0]
-            ego_pose = nusc.get("ego_pose", ego_pose_token)
-            calibrated_sensor = nusc.get("calibrated_sensor", sample_data["calibrated_sensor_token"])
+    for sample in tqdm(pedestrian_samples):
+        for camera in args.cameras:
+            datum = {}
+            human_ann = None
+            cnt = 0
+            cnt2 = 0
+            for ann in sample['anns']:
+                    sample_annotation = nusc.get('sample_annotation', ann)
+                    if sample_annotation['category_name'] == "human.pedestrian.stroller":
+                            continue
+                    if "human" in sample_annotation["category_name"] or "bicycle" in sample_annotation["category_name"] or "motorcycle" in sample_annotation["category_name"]:
+                        _, boxes, _ = nusc.get_sample_data(sample['data'][camera], box_vis_level=BoxVisibility.ANY, selected_anntokens=[ann])
+                        if boxes:
+                            cnt2 += 1
+                    if "human" in sample_annotation["category_name"]:
+                        _, boxes, _ = nusc.get_sample_data(sample['data'][camera], box_vis_level=BoxVisibility.ALL, selected_anntokens=[ann])
+                        if boxes and sample_annotation['visibility_token'] == '4':
+                            human_ann = sample_annotation
+                            cnt += 1
+            if cnt == cnt2 == 1:
+                sample_data = nusc.get("sample_data", sample['data'][camera])
+                ego_pose_token = nusc.field2token("ego_pose", "timestamp", sample["timestamp"])[0]
+                ego_pose = nusc.get("ego_pose", ego_pose_token)
+                calibrated_sensor = nusc.get("calibrated_sensor", sample_data["calibrated_sensor_token"])
 
-            datum["annotation"] = human_ann
-            datum["filename"] = sample_data["filename"]
-            datum["camera_translation"] = calibrated_sensor["translation"]
-            datum["camera_rotation"] = calibrated_sensor["rotation"]
-            datum["car_translation"] = ego_pose["translation"]
-            datum["car_rotation"] = ego_pose["rotation"]
-            single_pedestrians_data.append(datum)
+                datum["annotation"] = human_ann
+                datum["filename"] = sample_data["filename"]
+                datum["camera_translation"] = calibrated_sensor["translation"]
+                datum["camera_rotation"] = calibrated_sensor["rotation"]
+                datum["car_translation"] = ego_pose["translation"]
+                datum["car_rotation"] = ego_pose["rotation"]
+                single_pedestrians_data.append(datum)
 
     # Create the final dataset with the distance between the pedestrian and the camera, the distance bin and the filename
     dataset = []
